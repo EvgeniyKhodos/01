a0542dc5643368329cf4c43275ccb58460eaecd8
EvgeniyKhodos <en-hodos@mail.ru>
2020-02-16 22:11:10 +0700

    Adds First

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+  return <header className='header'>
+  <img src='https://cdn.pizap.com/pizapfiles/images/logo_maker_design_app04.jpg'></img>
+  </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+      <div>
+          <img src='https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'></img> 
+     </div>
+     <div>
+         ava + description
+     </div>
+     <div>
+        My posts
+     </div>
+   </div>
+}
+
+export default Profile;
