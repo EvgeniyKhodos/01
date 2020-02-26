1dfe40b76a01a24074e9722600a59e2f05cdee0f
EvgeniyKhodos <en-hodos@mail.ru>
2020-02-26 22:09:50 +0700

    Adds 37 Step Store, state, OOP

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, {subscribe, updateNewPostText} from "./redux/state";
-import {addPost} from "./redux/state";
+import store  from "./redux/state";
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
 
 let rerenderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state}
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>, document.getElementById('root'));
     // BrowserRouter - нужен, чтобы мы переключались независимо от введенных URL
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,87 +1,87 @@
-let rerenderEntireTree = () => {
-    console.log('Hello')
-}
-
-let state = {
-    dialogsPage: {
-        dialogs: [
-            {
-                id: 1,
-                name: 'Dimych',
-                img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
-            },
-            {
-                id: 2,
-                name: 'Andrew',
-                img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
-            },
-            {
-                id: 3,
-                name: 'Sveta',
-                img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
-            },
-            {
-                id: 4,
-                name: 'Sasha',
-                img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
-            },
-            {
-                id: 5,
-                name: 'Viktor',
-                img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
-            },
-            {
-                id: 6,
-                name: 'Valera',
-                img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
-            }
-        ],
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How is your it-kamasutra?'},
-            {id: 3, message: 'Yo'},
-            {id: 4, message: 'Yo'},
-            {id: 5, message: 'Yo'}
+let store = {
+    _state: {
+        dialogsPage: {
+            dialogs: [
+                {
+                    id: 1,
+                    name: 'Dimych',
+                    img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
+                },
+                {
+                    id: 2,
+                    name: 'Andrew',
+                    img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
+                },
+                {
+                    id: 3,
+                    name: 'Sveta',
+                    img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
+                },
+                {
+                    id: 4,
+                    name: 'Sasha',
+                    img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
+                },
+                {
+                    id: 5,
+                    name: 'Viktor',
+                    img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
+                },
+                {
+                    id: 6,
+                    name: 'Valera',
+                    img: 'https://static.mk.ru/upload/entities/2019/07/31/11/articles/detailPicture/74/2a/81/5e/f36f740bc1672e15668aaac619870fff.jpg'
+                }
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'Yo'},
+                {id: 4, message: 'Yo'},
+                {id: 5, message: 'Yo'}
+            ]
+        },
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi, how are you?', likesCount: 12},
+                {id: 2, message: 'It\'s my first post', likesCount: 11},
+                {id: 3, message: 'Three message', likesCount: 11},
+                {id: 4, message: 'Four', likesCount: 11},
+                {id: 5, message: 'Five', likesCount: 11}
+            ],
+            newPostText: 'it-kamasutra.com'
+        },
+        sidebar: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrew'},
+            {id: 3, name: 'Sveta'}
         ]
     },
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 12},
-            {id: 2, message: 'It\'s my first post', likesCount: 11},
-            {id: 3, message: 'Three message', likesCount: 11},
-            {id: 4, message: 'Four', likesCount: 11},
-            {id: 5, message: 'Five', likesCount: 11}
-        ],
-        newPostText: 'it-kamasutra.com'
+    getState () {
+        return this._state;
     },
-    sidebar: [
-        {id: 1, name: 'Dimych'},
-        {id: 2, name: 'Andrew'},
-        {id: 3, name: 'Sveta'}
-    ]
-};
-
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
+    _callSubscriber() {
+        console.log('Hello')
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        }
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer; // паттерн наблюдатель observer
     }
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText='';
-    rerenderEntireTree(state);
-};
-// Функция добавления постов, она же пушит содержимое в state и сразу перерисовывает
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-};
 // Функция для моментального обновления state при написании постов,
 // чтобы перерисовки происходила именно на его основе
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer; // паттерн наблюдатель observer
-}
-
-export default state;
+};
+export default store;
+window.store = store;
