186938ab5d35ac7ebc0bb8f1140b61304e116222
EvgeniyKhodos <en-hodos@mail.ru>
2020-02-27 23:32:58 +0700

    Adds 39 Step Action creator, action type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 import Profile from "../Profile";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
 
 const MyPosts = (props) => {
     let postsElements =
@@ -13,17 +14,17 @@ const MyPosts = (props) => {
     // создаем ссылку
 
     let addPost = () => {   //
-       // props.addPost();
-        props.dispatch( {type: 'ADD-POST'} )
+        // props.addPost();
+        props.dispatch(addPostActionCreator())
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
         //props.updateNewPostText(text);
-        let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text};
+        //let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text};
+        let action = updateNewPostTextActionCreator(text);
         props.dispatch(action)
     }
-
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
     _state: {
         dialogsPage: {
@@ -68,7 +71,7 @@ let store = {
         this._callSubscriber = observer; // паттерн наблюдатель observer
     },
     dispatch(action) {  // type: 'ADD-POST'
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 5,
                 message: this._state.profilePage.newPostText,
@@ -77,7 +80,7 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if (action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
         }
@@ -87,6 +90,8 @@ let store = {
 // чтобы перерисовки происходила именно на его основе
 };
 
+export const addPostActionCreator = () => ({type: ADD_POST});
+export const updateNewPostTextActionCreator = (text) => ({ type: UPDATE_NEW_POST_TEXT, newText: text });
 
 export default store;
 window.store = store;
