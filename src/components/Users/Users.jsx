2ce17d067b0cc61714145c80ae1de502265a5cb1
EvgeniyKhodos <en-hodos@mail.ru>
2020-03-13 12:05:04 +0700

    Adds 51 Step Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,51 +1,18 @@
 import React from "react";
 import styles from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/user.png';
 
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://static.probusiness.io/720x480c/n/05/9/36782619_1739036366166086_3461912368366223360_n.jpg',
-                    followed: false,
-                    fullName: 'Evgeny',
-                    status: 'I am happy',
-                    location: {city: 'Krasnoyrsk', country: 'Russia'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://static.probusiness.io/720x480c/n/05/9/36782619_1739036366166086_3461912368366223360_n.jpg',
-                    followed: true,
-                    fullName: 'Sasha',
-                    status: 'I am good',
-                    location: {city: 'Krasnoyrsk', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://static.probusiness.io/720x480c/n/05/9/36782619_1739036366166086_3461912368366223360_n.jpg',
-                    followed: false,
-                    fullName: 'Dmitry',
-                    status: '',
-                    location: {city: 'Krasnoyrsk', country: 'Russia'}
-                },
-                {
-                    id: 4,
-                    photoUrl: 'https://static.probusiness.io/720x480c/n/05/9/36782619_1739036366166086_3461912368366223360_n.jpg',
-                    followed: true,
-                    fullName: 'Anton',
-                    status: 'I am happy',
-                    location: {city: 'Krasnoyrsk', country: 'Russia'}
-                }
-            ]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => props.setUsers(response.data.items))
     }
-
     return <div>
         {
             props.users.map(u => <div key={u.id}>
             < span>
                 <div>
-                <img src={u.photoUrl} className={styles.userPhoto}/>
+                <img src={ u.photos.small != null ? u.photos.small : userPhoto } className={styles.userPhoto}/>
                 </div>
                 <div>
                     {u.followed
@@ -63,8 +30,8 @@ let Users = (props) => {
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>'u.location.country'</div>
+                        <div>'u.location.city'</div>
                     </span>
                     </span>
                 </div>
@@ -72,4 +39,5 @@ let Users = (props) => {
         }
     </div>
 };
+
 export default Users;
