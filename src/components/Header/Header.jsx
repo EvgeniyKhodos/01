b89a8784b2d3cdae327ecfafa7f4c70f8b8cacd4
EvgeniyKhodos <en-hodos@mail.ru>
2020-03-19 13:21:11 +0700

    Adds 60 Step

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
   return <header className={s.header}>
   <img src='https://awionline.org/sites/default/files/uploads/images/18-AWI-Logo-Hexagons-Green-Border_300x300.png'></img>
+
+    <div className={s.loginBlock}>
+      <NavLink to={'/login'}>Login</NavLink>
+    </div>
+
   </header>
 }
 
