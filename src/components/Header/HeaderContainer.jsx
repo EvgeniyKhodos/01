c37e15fdbcd0684b1e745eecb4336a07e7065fb9
EvgeniyKhodos <en-hodos@mail.ru>
2020-03-21 10:32:09 +0700

    Adds 60.1 Step

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import Header from "./Header";
 import * as axios from "axios";
+import {setAuthUserData} from "../../redux/auth-reducer";
+import {connect} from "react-redux";
 
 class HeaderContainer extends React.Component {
   componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-      withCredentials: true
-    })
-        .then(response => {
-debugger;
+    axios.get('https://social-network.samuraijs.com/api/1.0/auth/me', {withCredentials: true}).then(response => {
+ if (response.data.resultCode === 0) {
+     let {id, login, email} = response.data.data;
+     this.props.setAuthUserData(id, email, login);
+ }
         });
   }
 
@@ -17,5 +19,6 @@ debugger;
     }
 }
 
+const mapStateToProps = (state) => ({});
 
-export default HeaderContainer;
+export default connect(mapStateToProps, {setAuthUserData}) (HeaderContainer);
